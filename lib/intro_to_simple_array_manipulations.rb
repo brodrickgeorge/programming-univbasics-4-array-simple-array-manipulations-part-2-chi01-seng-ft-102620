e4f5b1bf9f9e8233c3257371056a8eed2f3aeda9
Brodrick George <brodrick.george@yahoo.com>
2020-10-12 02:32:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+
+def using_concat(my_favorite_things, more_favs)
+
+    my_favorite_things.concat(["sports cars", "flatiron school"])
+   
+end
+
+def using_insert(list_of_programming_languages, another_language)
+  
+  list_of_programming_languages.insert(4, "Python")
+
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq
+end
+
+def using_flatten(instruments)
+  instruments.flatten
+end
+
+def using_delete(instructors)
+  instructors.delete("Steven")
+end
